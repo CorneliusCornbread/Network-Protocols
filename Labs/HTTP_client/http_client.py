7d6d6e19e28f048498f0cc001423c7a77f266cc2
CorneliusCornbread <jrosebeckerdev@gmail.com>
2022-10-11 09:04:21 -0500

    working http client

--- Labs/HTTP_client/http_client.py
+++ Labs/HTTP_client/http_client.py
@@ -121,25 +121,36 @@ def do_http_exchange(host, port, resource, file_name):
     tcp_socket = setup_connection(host, port)
 
     # Request the resource and write the data to the file
-    request = f"""GET {resource} HTTP/1.1\r
-Host: {host}\r
-User-Agent: python-script\r
-Accept: */*\r\n"""
+    request = create_request(host, resource)
 
     bin_req = request.encode("ascii")
-    tcp_socket.send(bin_req)
+    tcp_socket.sendall(bin_req)
 
-    response_data = read_header(tcp_socket)
+    response_header = read_header(tcp_socket)
+    response_data = read_body(tcp_socket, response_header[3])
 
-
+    dump_bytes_to_file(f"body-{resource}.txt".replace('\\', '').replace('/', ''), response_data)
 
     # Don't forget to close the tcp_socket when finished
+    tcp_socket.close()
 
-    return 500  # Replace this "server error" with the actual status code
+    return response_header[1]
 
 
 # Define additional functions here as necessary
 # Don't forget docstrings and :author: tags
+
+
+def create_request(host, resource):
+    """
+    Creates a GET request based on the host and requested resource
+    :param host: host server to get resource from
+    :param resource: requested resource
+    :return: returns a GET request as a string
+    :author: Jack Rosenbecker
+    """
+    request = f"GET {resource} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+    return request
 
 
 def next_bytes(data_socket, n_bytes=1):
@@ -181,7 +192,7 @@ def read_header(data_socket):
 
     status_code = status[1]
     if 'uri-host' not in key_values.keys():
-        status_code = 400 # (Bad Request)
+        status_code = 400  # (Bad Request)
 
     return status[0], status_code, status[2], key_values
 
@@ -229,7 +240,7 @@ def parse_key_value(line_bytes: bytes) -> tuple:
     line_str = line_bytes.decode("ascii")
     line_list = line_str.split(":")
 
-    return line_list[0], ''.join(line_list[1::])
+    return line_list[0], (''.join(line_list[1::])).replace(' ', '')
 
 
 def is_chunked(key_values: dict) -> bool:
@@ -272,7 +283,8 @@ def read_body(data_socket, key_values):
     if is_chunked(key_values):
         size = read_chunk_length(data_socket)
         while size > 0:
-            message += next_bytes(data_socket, size)
+            for i in range(size):
+                message += next_bytes(data_socket)
 
             # This line throws away the carriage return line feed at the end of data
             next_bytes(data_socket, 2)
@@ -293,28 +305,12 @@ def read_chunk_length(data_socket):
     :author: Kade Swenson
     """
     size_in_bytes = read_line(data_socket)
-    size_in_text = size_in_bytes.decode("ASCII")
+    size_in_text = size_in_bytes.decode("ascii")
     size_in_int = int(size_in_text, 16)
     return size_in_int
 
 
-def read_data(data_socket, content_length):
-    """
-    Reads the data within an entire message.
-
-    :param: data_socket: The socket to read from. The data_socket argument should be an open tcp
-                    data connection (either a client socket or a server data socket), not a tcp
-                    server's listening socket.]
-    :param: content_length: The number of bytes in the message
-    :returns: data within the message.
-    :author: Kade Swenson
-    """
-
-    # After talking with the group this function is not needed because read_body does the function of this
-
-
-
-def dump_bytes_to_file(name, bytes):
+def dump_bytes_to_file(name: str, file_bytes: bytes):
     """
     Dumps bytes to a file.
     Will overwrite any files with the same name.
@@ -324,7 +320,7 @@ def dump_bytes_to_file(name, bytes):
     :author: Jack Rosenbecker
     """
     with open(name, 'wb') as f:
-        f.write(bytes)
+        f.write(file_bytes)
 
 
 main()
