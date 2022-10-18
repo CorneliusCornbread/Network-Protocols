39da63eedee24f465b1a964d152974d90143ba07
CorneliusCornbread <jrosebeckerdev@gmail.com>
2022-10-18 08:21:20 -0500

    Fix labs folder

---
+++ Labs/HTTP_client/http_client.py
@@ -0,0 +1,334 @@
+"""
+- NOTE: REPLACE 'N' Below with your section, year, and lab number
+- CS2911 - 011
+- Fall 2022
+- Lab 3
+- Names:
+  - Jack Rosenbecker
+  - Lucas Peterson
+  - Kade Swenson
+
+An HTTP client
+
+Introduction: (Describe the lab in your own words)
+Create an HTTP client that successfully gets data via HTTP from a remote server
+
+
+
+Summary: (Summarize your experience with the lab, what you learned, what you liked,what you disliked, and any suggestions you have for improvement)
+There were a lot of small details with this lab that needed to be worked out for this to work properly. Though once worked out it all came together.
+There is some behaviour as well that we didn't fully understand, like how we'd only sometimes receive a chunk of bytes at a time.
+Kinda challenging but also really fun, though what should be explained is how the sockets in python behave. I'm used to C# sockets, which will refuse to
+return execution until it has recieved the given number of bytes passed to it. Unlike python, which behaves a little differently if there's no data.
+"""
+
+import socket
+import re
+import ssl
+from typing import Dict
+
+
+def main():
+    """
+    Tests the client on a variety of resources
+    """
+
+    # These resource request should result in "Content-Length" data transfer
+    get_http_resource('https://www.httpvshttps.com/check.png', 'check.png')
+
+    # this resource request should result in "chunked" data transfer
+    get_http_resource('https://www.httpvshttps.com/', 'index.html')
+
+    # this resource request should result in "chunked" data transfer
+    # get_http_resource('https://www.youtube.com/', 'youtube.html')
+
+    # If you find fun examples of chunked or Content-Length pages, please share them with us!
+
+
+def get_http_resource(url, file_name):
+    """
+    Get an HTTP resource from a server
+           Parse the URL and call function to actually make the request.
+
+    :param url: full URL of the resource to get
+    :param file_name: name of file in which to store the retrieved resource
+
+    (do not modify this function)
+    """
+
+    protocol = 'https'
+    default_port = 443
+
+    # Parse the URL into its component parts using a regular expression.
+    if not url.startswith('https://'):
+        print('Request URL must start with https://')
+        return
+
+    url_match = re.search(protocol + '://([^/:]*)(:\d*)?(/.*)', url)
+    url_match_groups = url_match.groups() if url_match else []
+    #    print 'url_match_groups=',url_match_groups
+    if len(url_match_groups) == 3:
+        host_name = url_match_groups[0]
+        host_port = int(url_match_groups[1][1:]) if url_match_groups[1] else default_port
+        host_resource = url_match_groups[2]
+        print('host name = {0}, port = {1}, resource = {2}'.
+              format(host_name, host_port, host_resource))
+        status_string = do_http_exchange(host_name, host_port,
+                                         host_resource, file_name)
+        print('get_http_resource: URL="{0}", status="{1}"'.format(url, status_string))
+    else:
+        print('get_http_resource: URL parse failed, request not sent')
+
+
+def setup_connection(host, port):
+    """
+    Sets up the TCP connection to the given host at the given port using HTTPS (TLS)
+    :param str host: host name to connect to
+    :param int port: port number for the connection
+    :return: TCP socket
+
+    (do not modify this function)
+    """
+
+    # Set up the TCP connection
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((host, port))
+
+    # Wrap the socket in an SSL connection
+    context = ssl.create_default_context()
+    ssl_socket = context.wrap_socket(tcp_socket, server_hostname=host)
+    tcp_socket.close()
+
+    # Return the socket
+    return ssl_socket
+
+
+def do_http_exchange(host, port, resource, file_name):
+    """
+    Get an HTTP resource from a server
+
+    :param str host: the ASCII domain name or IP address of the server machine (i.e., host) to connect to
+    :param int port: port number to connect to on server host
+    :param str resource: the ASCII path/name of resource to get. This is everything in the URL after the domain name,
+           including the first /.
+    :param file_name: string (str) containing name of file in which to store the retrieved resource
+    :return: the status code
+    :rtype: int
+    :author: Jack Rosenbecker
+    """
+
+    # Setup the TCP connection
+    tcp_socket = setup_connection(host, port)
+
+    # Request the resource and write the data to the file
+    request = create_request(host, resource)
+
+    bin_req = request.encode("ascii")
+    tcp_socket.sendall(bin_req)
+
+    response_header = read_header(tcp_socket)
+    response_data = read_body(tcp_socket, response_header[3])
+
+    file_name == resource
+
+    if (resource == '\\'):
+        file_name = "index.html"
+
+    dump_bytes_to_file(file_name.replace('\\', '-').replace('/', '-'), response_data)
+
+    # Don't forget to close the tcp_socket when finished
+    tcp_socket.close()
+
+    return response_header[1]
+
+
+# Define additional functions here as necessary
+# Don't forget docstrings and :author: tags
+
+
+def create_request(host, resource):
+    """
+    Creates a GET request based on the host and requested resource
+    :param host: host server to get resource from
+    :param resource: requested resource
+    :return: returns a GET request as a string
+    :author: Jack Rosenbecker
+    """
+    request = f"GET {resource} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+    return request
+
+
+def next_bytes(data_socket, n_bytes=1):
+    """
+    Read the next n bytes from the socket data_socket.
+
+    Read the next n bytes from the sender, received over the network.
+    If the bytes have not yet arrived, this method blocks (waits)
+        until the bytes arrive.
+    If the sender is done sending and is waiting for your response, this method blocks indefinitely.
+
+    :param: data_socket: The socket to read from. The data_socket argument should be an open tcp
+                    data connection (either a client socket or a server data socket), not a tcp
+                    server's listening socket.
+    :param: n_bytes: The number of bytes returned by the method.
+    :return: the next n bytes, as a bytes object with multiple bytes in it
+    :author: Lucas Peterson
+    """
+    return data_socket.recv(n_bytes)
+
+
+def read_header(data_socket):
+    """
+    Reads header of HTTP response.
+
+    :param: data_socket: The socket to read from. The data_socket argument should be an open tcp data connection (
+    either a client socket or a server data socket), not a tcp server's listening socket.]
+    :return: tuple containing the version, status code, status message, and a dictionary of all key value pairs read in
+    the header.
+    :author: Lucas Peterson
+    """
+    status = parse_status_line(read_line(data_socket))
+    key_values = dict()
+    line = read_line(data_socket)
+    while len(line) > 0:
+        pair = parse_key_value(line)
+        key_values[pair[0]] = pair[1]
+        line = read_line(data_socket)
+
+    status_code = status[1]
+    if 'uri-host' not in key_values.keys():
+        status_code = 400  # (Bad Request)
+
+    return status[0], status_code, status[2], key_values
+
+
+def read_line(data_socket):
+    """
+    Reads bytes in line until a CRLF is hit
+
+    :param: data_socket: The socket to read from. The data_socket argument should be an open tcp
+                    data connection (either a client socket or a server data socket), not a tcp
+                    server's listening socket.]
+    :returns: bytes object containing all bytes in the line except the CRLF
+    :author: Lucas Peterson
+    """
+    byte = next_bytes(data_socket)
+    next_byte = next_bytes(data_socket)
+    line = bytes()
+    while byte + next_byte != b'\x0d\x0a':
+        line = line + byte
+        byte = next_byte
+        next_byte = next_bytes(data_socket)
+    return line
+
+
+def parse_status_line(line_bytes):
+    """
+    Reads the status line of the HTTP response.
+
+    :param: line_bytes: The bytes within the line
+    :returns: tuple containing the version, status code, and status message.
+    :author: Lucas Peterson
+    """
+    line = line_bytes.decode('ascii').split(' ')
+    return line[0], int(line[1]), line[2]
+
+
+def parse_key_value(line_bytes: bytes) -> tuple:
+    """
+    Reads a key value line of the HTTP response.
+
+    :param: line_bytes: The bytes within the line
+    :returns: tuple containing the key and value.
+    :author: Jack Rosenbecker
+    """
+    line_str = line_bytes.decode("ascii")
+    line_list = line_str.split(":")
+
+    return line_list[0], (''.join(line_list[1::])).replace(' ', '')
+
+
+def is_chunked(key_values: dict) -> bool:
+    """
+    Checks if response is chunked
+
+    :param: key_values: The dictionary of key value pairs in the response header
+    :returns: True if message is chunked, else False.
+    :author: Jack Rosenbecker
+    """
+    return key_values.get("Transfer-Encoding") == "chunked"
+
+
+def get_content_length(key_values: dict) -> int:
+    """
+    Gets the content length from key values
+
+    :param: key_values: The dictionary of key value pairs in the response header
+    :returns: The content length of the data.
+    :author: Jack Rosenbecker
+    """
+    header_length = key_values.get("Content-Length")
+    if header_length is None:
+        return -1
+
+    return int(header_length)
+
+
+def read_body(data_socket, key_values):
+    """
+    Reads the body
+
+    :param: data_socket: The socket to read from. The data_socket argument should be an open tcp
+                    data connection (either a client socket or a server data socket), not a tcp
+                    server's listening socket.]
+    :returns: the data within the body.
+    :author: Kade Swenson
+    """
+    message = b''
+    if is_chunked(key_values):
+        size = read_chunk_length(data_socket)
+        while size > 0:
+            for i in range(size):
+                message += next_bytes(data_socket)
+
+            # This line throws away the carriage return line feed at the end of data
+            next_bytes(data_socket, 2)
+            size = read_chunk_length(data_socket)
+    else:
+        length = get_content_length(key_values)
+
+        for i in range(length):
+            message += next_bytes(data_socket)
+    return message
+
+
+def read_chunk_length(data_socket):
+    """
+    Reads the chunk length
+
+    :param: data_socket: The socket to read from. The data_socket argument should be an open tcp
+                    data connection (either a client socket or a server data socket), not a tcp
+                    server's listening socket.]
+    :returns: the length of the chunk
+    :author: Kade Swenson
+    """
+    size_in_bytes = read_line(data_socket)
+    size_in_text = size_in_bytes.decode("ascii")
+    size_in_int = int(size_in_text, 16)
+    return size_in_int
+
+
+def dump_bytes_to_file(name: str, file_bytes: bytes):
+    """
+    Dumps bytes to a file.
+    Will overwrite any files with the same name.
+
+    :param: name: name of the file with the extension
+    :param: bytes: actual contents of the file to write
+    :author: Jack Rosenbecker
+    """
+    with open(name, 'wb') as f:
+        f.write(file_bytes)
+
+
+main()
---
+++ Labs/HTTP_server/http_server.py
@@ -0,0 +1,340 @@
+"""
+- NOTE: REPLACE 'N' Below with your section, year, and lab number
+- CS2911 - 0NN
+- Fall 2022
+- Lab 4
+- Names:
+  - Lucas Peterson
+  - Jack Rosenbecker
+  - Kade Swenson
+
+An HTTP server
+
+Introduction: (Describe the lab in your own words)
+
+
+
+
+Summary: (Summarize your experience with the lab, what you learned, what you liked,what you disliked, and any suggestions you have for improvement)
+
+
+
+
+
+"""
+
+from email import message
+import re
+import socket
+import threading
+import os
+import mimetypes
+import datetime
+from typing import Dict
+
+HTTP_TEMPLATE = "HTTP/{version} {response_code} {message}\r\n" \
+                "Date: {date}\r\n" \
+                "Content-Length: {length}\r\n" \
+                "Connection: {connection}\r\n" \
+                "Content-Type: {content_type}\r\n\r\n"
+
+
+def main():
+    """ Start the server """
+    http_server_setup(8083)
+
+
+def http_server_setup(port):
+    """
+    Start the HTTP server
+    - Open the listening socket
+    - Accept connections and spawn processes to handle requests
+
+    :param port: listening port number
+    """
+
+    num_connections = 10
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_address = ('', port)
+    server_socket.bind(listen_address)
+    server_socket.listen(num_connections)
+    try:
+        while True:
+            request_socket, request_address = server_socket.accept()
+            print('connection from {0} {1}'.format(request_address[0], request_address[1]))
+            # Create a new thread, and set up the handle_request method and its argument (in a tuple)
+            request_handler = threading.Thread(target=handle_request, args=(request_socket,))
+            # Start the request handler thread.
+            request_handler.start()
+            # Just for information, display the running threads (including this main one)
+            print('threads: ', threading.enumerate())
+    # Set up so a Ctrl-C should terminate the server; this may have some problems on Windows
+    except:
+        print("HTTP server exiting . . .")
+        print('threads: ', threading.enumerate())
+        server_socket.close()
+
+
+def handle_request(request_socket):
+    """
+    Handle a single HTTP request, running on a newly started thread.
+
+    Closes request socket after sending response.
+
+    Should include a response header indicating NO persistent connection
+
+    :param request_socket: socket representing TCP connection from the HTTP client_socket
+    :return: None
+    :author: Lucas Peterson
+    """
+    try:
+        req = request(request_socket)
+        res = response(req)
+        res.send(request_socket, req.resource)
+    except:
+        print("HTTP server exiting . . .")
+        print('threads: ', threading.enumerate())
+
+    request_socket.close()
+    
+
+
+# Utility functions
+# You may use these functions to simplify your code.
+
+
+class request:
+    """
+    Constructor reads a request header from a socket, and stores its context as the type, resource, version,
+    and headers (key values) instance variables.
+
+    :author: Lucas Peterson
+    """
+
+    def __init__(self, request_socket: socket):
+        self.read_header(request_socket)
+
+    def next_bytes(self, request_socket, n_bytes=1):
+        """
+        Read the next n bytes from the socket data_socket.
+
+        Read the next n bytes from the sender, received over the network.
+        If the bytes have not yet arrived, this method blocks (waits)
+            until the bytes arrive.
+        If the sender is done sending and is waiting for your response, this method blocks indefinitely.
+
+        :param: request_socket: The socket to read from. The request_socket argument should be an open tcp
+            request connection (either a client socket or a server data socket), not a tcp server's
+            listening socket.]
+        :param: n_bytes: The number of bytes returned by the method.
+        :return: the next n bytes, as a bytes object with multiple bytes in it
+        :author: Lucas Peterson
+        """
+        n = 0
+        message = b''
+        while n < n_bytes:
+            message += request_socket.recv(1)
+            n += 1
+        return message
+
+    def read_header(self, request_socket):
+        """
+        Reads header of HTTP request and assigns values to request type, resource, version, and key values.
+
+        :param request_socket: The socket to read from. The request_socket argument should be an open tcp
+            request connection (either a client socket or a server data socket), not a tcp server's
+            listening socket.]
+        :author: Lucas Peterson
+        """
+        line_bytes = self.read_line(request_socket)
+        request = self.parse_request_line(line_bytes)
+        self.headers = dict()
+        line = self.read_line(request_socket)
+        while len(line) > 0:
+            pair = self.parse_key_value(line)
+            self.headers[pair[0]] = pair[1]
+            line = self.read_line(request_socket)
+
+        self.type = request[0]
+        self.resource = request[1].removeprefix('/')
+        self.version = request[2].split('/')[1]
+
+    def read_line(self, request_socket):
+        """
+        Reads bytes in line until a CRLF is hit
+
+        :param request_socket: The socket to read from. The request_socket argument should be an open tcp
+            request connection (either a client socket or a server data socket), not a tcp server's
+            listening socket.]
+        :returns: bytes object containing all bytes in the line except the CRLF
+        :author: Lucas Peterson
+        """
+        byte = self.next_bytes(request_socket)
+        next_byte = self.next_bytes(request_socket)
+        line = bytes()
+        while byte + next_byte != b'\x0d\x0a':
+            line = line + byte
+            byte = next_byte
+            next_byte = self.next_bytes(request_socket)
+        return line
+
+    def parse_request_line(self, line_bytes):
+        """
+        Reads the status line of the HTTP response.
+
+        :param line_bytes: The bytes within the line
+        :returns: tuple containing the type, resource, and version.
+        :author: Lucas Peterson
+        """
+        line = line_bytes.decode('ascii').split(' ')
+        return line[0], line[1], line[2]
+
+    def parse_key_value(self, line_bytes):
+        """
+        Reads a key value line of the HTTP request.
+
+        :param line_bytes: The bytes within the line
+        :returns: tuple containing the key and value.
+        :author: Jack Rosenbecker
+        """
+        line_str = line_bytes.decode("ascii")
+        line_list = line_str.split(":")
+
+        return line_list[0], ''.join(line_list[1::])
+
+
+def get_time():
+    """
+    :author: Kade Swenson
+    """
+    timestamp = datetime.datetime.utcnow()
+    timestamp = timestamp.strftime('%a, %d %b %Y %H:%M:%S GMT')
+    return timestamp.encode("ascii")
+
+
+class response:
+    """
+    Creates a server response based on a clients request. Once the response is created
+    it can be sent to the client using the send() method.
+
+    :authors: Lucas Peterson, Kade Swenson, and Jack Rosenbecker
+    """
+
+    def __init__(self, client_request: request):
+        self.version = '1.1'
+        status_tuple = self.get_status(client_request)
+        self.status_code = status_tuple[0]
+        self.status = status_tuple[1]
+        self.headers = self.add_headers(client_request)
+
+    def get_status(self, client_request: request) -> tuple:
+        """
+        :author: Kade Swenson
+        """
+        if client_request.version != "1.1":
+            return 400, 'Bad Request'
+        if get_file_size(client_request.resource) is None:
+            return 404, 'Not Found'
+        return 200, 'Okay'
+
+    def add_headers(self, client_request: request) -> Dict:
+        """
+        :author: Kade Swenson
+        """
+        headers = dict()
+        headers['Date'] = get_time()
+        headers['Connection'] = "close"
+        headers['Content-Type'] = get_mime_type(client_request.resource)
+        headers['Content-Length'] = get_file_size(client_request.resource)
+        return headers
+
+    def send(self, server_socket: socket, resource: str):
+        """
+        Creates an HTTP header plus body and sends it to the connected address.
+
+        :param server_socket: The socket handling our current connection.
+        :param resource: The requested resource from the client.
+        :author: Jack Rosenbecker
+        """
+        headers = self.headers
+        if len(headers) == 0:
+            raise Exception(
+                "Response must have its headers setup before it can send. Make sure you call add_headers() before send.")
+
+        print(f"Serving resource \'{resource}\'")
+
+        header_str = HTTP_TEMPLATE.format(
+            version=self.version,
+            response_code=self.status_code,
+            message=self.status,
+            date=headers['Date'],
+            length=headers['Content-Length'],
+            connection=headers['Connection'],
+            content_type=headers['Content-Type']
+        )
+
+        header_bin = header_str.encode('ascii')
+        server_socket.send(header_bin)
+        file_bytes = bytes()
+
+        if self.status_code == 200:
+            file_bytes = get_file_bytes(resource)
+        else:
+            print(f"Requested resource: \'{resource}\', not found at expected location: {os.path.abspath(resource)}")
+
+        server_socket.send(file_bytes)  # Resource data
+        server_socket.send('\r\n\r\n'.encode('ascii'))  # End of stream
+
+
+def get_mime_type(file_path: str):
+    """
+    Try to guess the MIME type of a file (resource), given its path (primarily its file extension)
+
+    :param file_path: string containing path to (resource) file, such as './abc.html'
+    :return: If successful in guessing the MIME type, a string representing the content type, such as 'text/html'
+             Otherwise, None
+    :rtype: str or None
+    """
+    abs_file_path = os.path.abspath(file_path)
+
+    mime_type_and_encoding = mimetypes.guess_type(abs_file_path)
+    mime_type = mime_type_and_encoding[0]
+    return mime_type
+
+
+def get_file_size(file_path: str):
+    """
+    Try to get the size of a file (resource) as number of bytes, given its path
+
+    :param file_path: string containing path to (resource) file, such as './abc.html'
+    :return: If file_path designates a normal file, an integer value representing the the file size in bytes
+             Otherwise (no such file, or path is not a file), None
+    :rtype: int or None
+    """
+
+    # Initially, assume file does not exist
+    file_size = None
+    abs_file_path = os.path.abspath(file_path)
+
+    if os.path.isfile(abs_file_path):
+        file_size = os.stat(abs_file_path).st_size
+    return file_size
+
+
+def get_file_bytes(file_path: str) -> bytes:
+    """
+    Get a file from the given path and return its bytes.
+
+    :param file_path: Relative file path string.
+    :return: The bytes of the given file.
+    """
+    abs_file_path = os.path.abspath(file_path)
+    size = get_file_size(file_path)
+
+    file = open(abs_file_path, "rb")
+    return file.read(size)
+
+
+main()
+
+# Replace this line with your comments on the lab
